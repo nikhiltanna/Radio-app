bba450d43704d1d8f5caf037efe069c58b060f99
nikhiltanna <46378893+nikhiltanna@users.noreply.github.com>
2023-05-05 08:23:26 +0530

    Added

---
+++ radio-player/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Radio from './Radio';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Super-Duper Radio Player </h1>
+        <h2>Pick a genre,choose a station ,start listening</h2>
+          <Radio />
+    </div>
+  );
+}
+
+export default App;
